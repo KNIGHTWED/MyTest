f2fe3c4bea63fd4162b9ed9ae5da15d30ce3694a
김정민 <jungmin0322@gmail.com>
2022-06-29 02:03:55 +0900

    0629

--- TestC.c
+++ TestC.c
@@ -1,11 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS  
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 int main(){
-    char str[10];
+    char *s1 = "0123456789";
+    char *s2 = "a234";
+    bool answer = true;
 
-    scanf("%s", str);
-    printf("%s\n", str);
-
+    for (int i = 0; i < strlen(s2); i++)
+    {
+        printf("%d ", *(s2+i));
+    }
+    
     return 0;
 }
